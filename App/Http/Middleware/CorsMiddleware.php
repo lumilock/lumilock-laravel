cffce73b918231471f44ca247cd2cd11d11479f3
lumilock <thibaud.perrin@outlook.com>
2021-02-20 20:06:32 +0100

    update routes

--- App/Http/Middleware/CorsMiddleware.php
+++ App/Http/Middleware/CorsMiddleware.php
@@ -24,7 +24,7 @@ class CorsMiddleware
 
             $headers = [
                 'Access-Control-Allow-Origin'      => '*',
-                'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE', 'PATCH',
+                'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE, PATCH',
                 'Access-Control-Allow-Credentials' => 'true',
                 'Access-Control-Max-Age'           => '86400',
                 'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
@@ -34,7 +34,7 @@ class CorsMiddleware
                 return response()->json('{"method":"OPTIONS"}', 200, $headers);
             }
 
-            if (in_array($request->method(), ['POST', 'PUT', 'PATCH']) && $request->isJson()) {
+            if (in_array($request->method(), ['POST', 'PUT', 'PATCH', 'DELETE']) && $request->isJson()) {
                 $data = $request->json()->all();
                 $request->request->replace(is_array($data) ? $data : []);
             }
--- Routes/web.php
+++ Routes/web.php
@@ -17,34 +17,91 @@ $this->app->router->group(
     ],
     function ($router) {
         $router->group(['prefix' => 'auth'], function () use ($router) {
-            // Matches "/api/register
-            $router->post('register', 'AuthController@register');
+            // ROUTE : api/auth/...
 
-            // Matches "/api/login
+            // Auth functions
             $router->post('login', 'AuthController@login');
-
-            // Matches "/api/logout
             $router->post('logout', 'AuthController@logout');
-
-            // Matches "/api/profile
-            $router->get('profile', 'UserController@profile');
-
-            // Matches "/api/users/1 
-            //get one user by id
-            $router->get('users/{id}', 'UserController@singleUser');
-
-            // Matches "/api/check 
-            //get user by token
+            $router->post('register', 'AuthController@register'); // ! deprecated
             $router->get('check', 'AuthController@check');
 
-            // Matches "/api/users
+            // Profile
+            $router->get('profile', 'UserController@profile');
+            $router->put('profile', function () {
+                return 'Not implemented : post /api/auth/profile, update the user profile.';
+            });
+            $router->get('profile/tokens', function () {
+                return 'Not implemented : get /api/auth/profile/tokens = all tokens of the auth user.';
+            });
+            $router->delete('profile/tokens', function () {
+                return 'Not implemented : delete /api/auth/profile/tokens = remove all tokens of the auth user and logout.';
+            });
+            $router->delete('profile/tokens/{tokenId:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', function () {
+                return 'Not implemented : delete /api/auth/profile/tokens/{tokenId} = remove specific token of the auth user and logout (but not current).';
+            });
+
+            // Users
             $router->get('users', 'UserController@allUsers');
+            $router->get('users/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', 'UserController@singleUser');
+            $router->post('users', 'AuthController@register');
+            $router->put('users/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', function () {
+                return 'Not implemented : put /api/auth/users/{id} = update a specific user.';
+            });
+            $router->delete('users/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', function () {
+                return 'Not implemented : delete /api/auth/users/{id} = delete a specific user.';
+            });
+            $router->get('users/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}/rights', function () {
+                return 'Not implemented : get /api/auth/users/{id}/rights = get all rights by services from a user.';
+            });
+            $router->put('users/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}/rights', function () {
+                return 'Not implemented : put /api/auth/users/{id}/rights = update all rights by services from a user.';
+            });
+
+            // Services
+            $router->get('services', function () {
+                return 'Not implemented : get /api/auth/services = get all services.';
+            });
+            $router->post('services', 'ServiceController@singleService');
+            $router->get('services/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', function () {
+                return 'Not implemented : get /api/auth/services/{id} = get specific service.';
+            });
+            $router->put('services/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', function () {
+                return 'Not implemented : put /api/auth/services/{id} = update specific service.';
+            });
+            $router->delete('services/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', function () {
+                return 'Not implemented : delete /api/auth/services/{id} = delete specific service.';
+            });
+            $router->get('services/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}/permissions', function () {
+                return 'Not implemented : get /api/auth/services/{id}/permissions = get all permission of a service.';
+            });
+
+            // Keys
+            $router->get('keys', function () {
+                return 'Not implemented : get /api/auth/keys = get all api keys.';
+            });
+            $router->post('keys', function () {
+                return 'Not implemented : post /api/auth/keys = create a new api key.';
+            });
+            $router->get('keys/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', function () {
+                return 'Not implemented : get /api/auth/keys/{id} = get a specific api keys.';
+            });
+            $router->delete('keys/{id:[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}}', function () {
+                return 'Not implemented : delete /api/auth/keys/{id} = remove a specific api keys.';
+            });
 
             // Matches "/api/install
             $router->get('install', 'InstallController@installTest');
 
-            // Matches "/api/auth/services/
-            $router->post('services', 'ServiceController@singleService');
+
+            $notFoundController = function () {
+                return 'Not Found';
+            };
+
+            $router->get('/{route:.*}/', $notFoundController);
+            $router->post('/{route:.*}/', $notFoundController);
+            $router->put('/{route:.*}/', $notFoundController);
+            $router->patch('/{route:.*}/', $notFoundController);
+            $router->delete('/{route:.*}/', $notFoundController);
         });
     }
 );
